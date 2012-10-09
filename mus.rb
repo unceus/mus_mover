2a43bd1743f257c54419461bd976d1fc5babe695
Michael Joseph <michael@unce.us>
2012-10-09 10:04:33 -0400

    stable

--- mus.rb
+++ mus.rb
@@ -1,2 +1,85 @@
 #!/bin/ruby
-puts 'derp'
+require 'rubygems'
+require 'fileutils'
+require 'commander/import'
+
+class MusicMatcher
+  DEFAULT_MUSIC_DIR = '/Music/Incoming'
+  DEFAULT_THRESHHOLD = 30
+  DS = '/'
+  def self.ls(dir, threshhold)
+  threshhold = threshhold || DEFAULT_THRESHHOLD
+  dir = dir || Dir.getwd
+    Dir.new(dir).each do |e|
+      if is_directory?(dir, e)
+        if is_album?(dir,e) and created_at_within_threshhold?(dir, e, threshhold)
+          puts e
+        end
+      end
+    end
+  end
+# 
+  def self.move(dir, to, threshhold)
+    threshhold = threshhold || DEFAULT_THRESHHOLD
+    dir = dir || Dir.getwd
+    to = to || DEFAULT_MUSIC_DIR
+
+    Dir.new(dir).each do |e|
+      if is_directory?(dir, e)
+        if is_album?(dir,e) and created_at_within_threshhold?(dir, e, threshhold)
+          move_file(dir + DS + e, to)
+          #puts "#{{ from: dir + DS + e, to: to }}"
+        end
+      end
+    end
+  end
+
+  def self.get_threshhold
+    DEFAULT_THRESHHOLD
+  end
+
+  protected
+
+  def self.move_file(from, to)
+    FileUtils.mv from, to
+  end
+
+  def self.is_directory?(path, entry)
+    File.directory?(path + DS + entry)
+  end
+
+  def self.created_at_within_threshhold?(path, entry, threshhold = 30)
+    #puts File.mtime(path + DS + entry)
+    #puts (Time.now - File.ctime(path + DS + entry)).divmod(86400)
+    (Time.now - File.mtime(path + DS + entry)).divmod(86400)[0] > threshhold
+  end
+  def self.is_album?(path, entry)
+    Dir.chdir(path + DS + entry)
+    Dir.glob("*.{mp3,flac}").length > 3
+  end
+end
+
+program :name, 'Music Mover'
+program :version, '0.0.2'
+program :description, 'Command that moves old music.'
+
+command :ls do |c|
+  c.syntax = 'mus ls --dir [dir]'
+  c.description = 'Lists music to be moved'
+  c.option '--dir STRING', String, 'Specify a directory to search (optional)'
+  c.option '--days INTEGER', Integer, 'Specify a threshhold in days (optional)'
+  c.action do |args, options|
+      MusicMatcher.ls(options.dir, options.days)
+  end
+end
+
+command :move do |c|
+  c.syntax = 'mus move --dir [dir]'
+  c.description = 'Moves music'
+  c.option '--dir STRING', String, 'Specify a directory to search (optional)'
+  c.option '--to STRING', String, 'Specify a directory to move files to (optional)'
+  c.option '--days INTEGER', Integer, 'Specify a threshhold in days (optional)'
+  c.action do |args, options|
+      MusicMatcher.move(options.dir, options.to, options.days)
+  end
+end
